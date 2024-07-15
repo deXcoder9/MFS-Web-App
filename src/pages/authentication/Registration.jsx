cc54ed904c9c3533eea0ea330f58dede1f709b93
deXcoder9_ <dhrubothejuniorprogrammer@gmail.com>
2024-07-16 02:12:54 +0600

    [authentication]registration page added

---
+++ src/pages/authentication/Registration.jsx
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+import "./style.css"
+
+const Registration = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+    const formData = {
+        ...data,
+        status: "pending"
+    }
+    console.log(formData)
+  };
+
+  return (
+    <div className="max-w-[700px] mx-auto  bg-gray-700 " >
+      <form onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col space-y-7 px-16 py-14 "
+      >
+        {/* register your input into the hook by invoking the "register" function */}
+        <input type="text" {...register("name", {required: true}) } placeholder="Name" />
+        <input type="number"  {...register("pin", {required: true})} placeholder="PIN" />
+        <input type="number" {...register("mobile", {required: true})} placeholder="Mobile Number" />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input type="email" {...register("email", { required: true })} placeholder="Email" />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span className="text-red-600">This field is required</span>}
+
+        <input 
+        type="submit"
+        className="btn btn-primary"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
