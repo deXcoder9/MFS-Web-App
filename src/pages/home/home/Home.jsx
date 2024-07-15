5b12a0e1b1a5c663e3a98015c858733efad8e802
deXcoder9_ <dhrubothejuniorprogrammer@gmail.com>
2024-07-16 02:11:43 +0600

    react template and packages added

---
+++ src/pages/home/home/Home.jsx
@@ -0,0 +1,15 @@
+// import Navbar from "../../../shared/navbar/Navbar";
+
+import Registration from "../../authentication/Registration";
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* <Navbar></Navbar> */}
+            <Registration></Registration>
+        </div>
+    );
+};
+
+export default Home;
